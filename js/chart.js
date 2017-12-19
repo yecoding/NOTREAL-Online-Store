1657a6299ecdc81fcac13025b2369c4ad152be98
Ye Wang <wangye.tju@gmail.com>
2017-12-19 11:00:09 -0500

    Add price calculation function and contact page

--- js/chart.js
+++ js/chart.js
@@ -6,48 +6,48 @@ chart.controller('chartController', function($scope){
 			image: 'graphics/items/twocatsplate.jpg',
 			description:'A 100mm x 100mm mini plate with two cuties saying hi to you. The box-shape plate is perfect for small jwellery storage',
 			price: 60,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		{
 			title: 'Mom Cat Plate',
 			image: 'graphics/items/momcatplate.jpg',
 			description:'A mom cat is with her kid cat standing on the plate.',
 			price: 50,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		{
 			title: 'Elephants Plate',
 			image: 'graphics/items/elephantplate.jpg',
 			description:'Many cultures revere elephants as a symbol of honor and good luck.',
 			price: 50,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		{
 			title: 'Panda Brooch',
 			image: 'graphics/items/pandabrooch.jpg',
 			description:'Pink might be the best color for sunglasses of a panda.',
 			price: 25,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		{
 			title: 'Plants Girl Shadowbox',
 			image: 'graphics/items/plantsgirlshadowbox.jpg',
 			description:'This is a customized shadowbox for my dear friend Agnes who is a big fan of succulents and cats.',
 			price: 116,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		{
 			title: 'Lotus Incense Holder',
 			image: 'graphics/items/lotusincenseholder.jpg',
 			description:'A lotus incense holder, all handpainted.',
 			price: 80,
-			active:false,
-			select: false
+			select:false,
+			status:'Add to Chart'
 		},
 		]
 
@@ -59,17 +59,20 @@ chart.controller('chartController', function($scope){
 			if(e.select){
 				$scope.total = $scope.total + e.price;
 				$scope.count = $scope.count + 1;
+				e.status = 'Remove item';
 			}else{
-				$scope.total = $scope.total - e,price;
 				$scope.count = $scope.count - 1;
+				$scope.total = $scope.total - e.price;
+				e.status = 'Add to Chart';
 			}
-			return $scope.total;
 		}
 
-		$scope.showDetails = function(e){
-			var x= document.getElementById('')
+		$scope.removeItem = function(e){
+			e.select = !e.select;
+			$scope.count = $scope.count - 1;
+			$scope.total = $scope.total - e.price;
+			e.status = 'Add to Chart';
 		}
-	
 
 
 });
